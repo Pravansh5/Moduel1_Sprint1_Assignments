7315a11c420ad900d11dc96addc8cd8209ee644c
Pravansh5 <pravanshpatel6.pp@gmail.com>
2025-02-22 23:03:15 +0530

    arrayManipulations

---
+++ arrayManipulation.js
@@ -0,0 +1,13 @@
+let temperatureArr=[];
+temperatureArr.push(20);
+temperatureArr.push(22);
+temperatureArr.push(24);
+temperatureArr.push(23);
+temperatureArr.push(24);
+temperatureArr.push(20);
+temperatureArr.push(19);
+
+let fourthDayTemp=temperatureArr[3];
+fourthDayTemp+=2;
+temperatureArr[3]=fourthDayTemp;
+console.log(temperatureArr);
