7f1c3b2d3033715abcf9c537dface28ddb26b0bb
Pravansh5 <pravanshpatel6.pp@gmail.com>
2025-02-22 22:29:16 +0530

    DailyTemperature

---
+++ DailyTemperature.js
@@ -0,0 +1,14 @@
+let temperatureArr=[];
+temperatureArr.push(23);
+temperatureArr.push(22);
+temperatureArr.push(21);
+temperatureArr.push(23);
+temperatureArr.push(24);
+temperatureArr.push(26);
+temperatureArr.push(23);
+
+let fourthDayTemp=temperatureArr[3];
+fourthDayTemp+=2;
+temperatureArr[3]=fourthDayTemp;
+console.log(temperatureArr);
+
