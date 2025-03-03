8a4f6b63605be649256eceef52098bffd6855d41
Pravansh5 <pravanshpatel6.pp@gmail.com>
2025-03-03 15:44:02 +0530

    Dynamic User Role Message with Object Properties Updated

--- dynamicUserRoleObjectProperties.js
+++ dynamicUserRoleObjectProperties.js
@@ -1,12 +1,18 @@
-let user = { name: "Alice", role: "user", active: true };
-if(user.role==="admin" && user.active==true){
-    console.log("Admin Access Granted!");
-}else if(user.role==="admin" && user.active==false){
-    console.log("Admin Access Revoked")
-}else if(user.role==="user" && user.active==true){
-    console.log("User Access Granted")
-}else if(user.role=="user" && user.active==false){
-    console.log("User Access Revoked");
-}else{
-    console.log("Access Denied");
-}
+let user = { name: "Alice", role: "user", active: false };
+let dynamicMsg= user.role==="admin"?(user.active==true?"Admin Access Granted!":"Admin Access Revoked")
+:user.role==="user"?(user.active==true?"User Access Granted!":"User Access Revoked")
+:"Access Denied"
+console.log(dynamicMsg)
+
+
+// if(user.role==="admin" && user.active==true){
+//     console.log("Admin Access Granted!");
+// }else if(user.role==="admin" && user.active==false){
+//     console.log("Admin Access Revoked")
+// }else if(user.role==="user" && user.active==true){
+//     console.log("User Access Granted")
+// }else if(user.role=="user" && user.active==false){
+//     console.log("User Access Revoked");
+// }else{
+//     console.log("Access Denied");
+// }
